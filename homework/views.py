155037fcc41229b28a7c203d4bf0b877c7d8c789
Nurzada Diusheeva <nurzada1995@gmail.com>
2022-04-28 01:33:03 +0600

    added todo list in test page

--- homework/views.py
+++ homework/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import ToDo
 
 def homework(request):
     return render(request, "index.html")
@@ -8,7 +9,8 @@ def todo_list(request):
     return render(request, "homework.html")
 
 def test(request):
-    return render(request, "test.html")
+    todo_list = ToDo.objects.all()
+    return render(request, "test.html", {"todo_list": todo_list})
 
 def second(request):
     return HttpResponse("Test 2  page")
